24bc274ebc23aa8a63d462afb134b4504893df60
ryunalore <96705308+ryunalore@users.noreply.github.com>
2022-12-27 10:58:23 +0100

    tableView demo adapted

--- src/main/java/fhtw/trafficmonitor/Controller.java
+++ src/main/java/fhtw/trafficmonitor/Controller.java
@@ -18,34 +18,41 @@ public class Controller implements Initializable {
         private TableView<LineRecord> tableViewDemo;
 
         @FXML
+        private TableColumn<LineRecord, String> transportType;
+
+        @FXML
         private TableColumn<LineRecord, String> line;
+
+        @FXML
+        private TableColumn<LineRecord, String> stationName;
 
         @FXML
         private TableColumn<LineRecord, String> towards;
 
         @FXML
-        private TableColumn<LineRecord, String> departureTimePlanned;
+        private TableColumn<LineRecord, String> departureTimePlanned1;
+
+        @FXML
+        private TableColumn<LineRecord, String> departureTimePlanned2;
 
         ObservableList<LineRecord> list = FXCollections.observableArrayList(
-                new LineRecord("1", "Stefan-Fadinger-Platz", "2022-12-19T23:53:30.000+0100"),
-                new LineRecord("1", "Stefan-Fadinger-Platz", "2022-12-20T00:05:30.000+0100"),
-                new LineRecord("1","Prater Hauptallee","2022-12-20T00:02:00.000+0100"),
-                new LineRecord("2","Wexstraße, Betriebsbhf. Brigittenau","2022-12-20T00:15:00.000+0100"),
-                new LineRecord("2","Wexstraße, Betriebsbhf. Brigittenau","2022-12-20T00:30:00.000+0100"),
-                new LineRecord("2","Friedrich-Engels-Platz","2022-12-19T23:39:00.000+0100"),
-                new LineRecord("2","Friedrich-Engels-Platz","2022-12-19T23:54:00.000+0100"),
-                new LineRecord("2","Dornbach", "2022-12-20T00:00:07.000+0100"),
-                new LineRecord("2","Dornbach", "2022-12-20T00:24:12.000+0100")
-
+                new LineRecord("Tram","1","Schwedenplatz U", "Stefan-Fadinger-Platz", "2022-12-19T23:53:30.000+0100","2022-12-20T00:05:30.000+0100"),
+                new LineRecord("Tram","1","Schwedenplatz U","Prater Hauptallee","2022-12-20T00:02:00.000+0100",""),
+                new LineRecord("Tram","2","Schwedenplatz U","Wexstraße, Betriebsbhf. Brigittenau","2022-12-20T00:15:00.000+0100","2022-12-20T00:30:00.000+0100"),
+                new LineRecord("Tram","2","Schwedenplatz U","Friedrich-Engels-Platz","2022-12-19T23:39:00.000+0100","2022-12-19T23:54:00.000+0100"),
+                new LineRecord("Tram","2","Schwedenplatz U","Dornbach", "2022-12-20T00:00:07.000+0100", "2022-12-20T00:24:12.000+0100")
         );
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        transportType.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineTransportType"));
         line.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineName"));
+        stationName.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineStationName"));
         towards.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineTowards"));
-        departureTimePlanned.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineDepartureTimePlanned"));
+        departureTimePlanned1.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineDepartureTimePlanned1"));
+        departureTimePlanned2.setCellValueFactory(new PropertyValueFactory<LineRecord, String>("lineDepartureTimePlanned2"));
 
         tableViewDemo.setItems(list);
 
--- src/main/java/fhtw/trafficmonitor/LineRecord.java
+++ src/main/java/fhtw/trafficmonitor/LineRecord.java
@@ -1,39 +1,47 @@
 package fhtw.trafficmonitor;
 
 public class LineRecord {
+    private String lineTransportType;
     private String lineName;
+    private String lineStationName;
     private String lineTowards;
-    private String lineDepartureTimePlanned;
+    private String lineDepartureTimePlanned1;
+    private String lineDepartureTimePlanned2;
     private String lineType;
     private String lineId;
 
-
-    public LineRecord(String lineName, String lineTowards, String lineDepartureTimePlanned) {
+    public LineRecord(String lineTransportType, String lineName, String lineStationName, String lineTowards, String lineDepartureTimePlanned1, String lineDepartureTimePlanned2) {
+        this.lineTransportType = lineTransportType;
         this.lineName = lineName;
+        this.lineStationName = lineStationName;
         this.lineTowards = lineTowards;
-        this.lineDepartureTimePlanned = lineDepartureTimePlanned;
+        this.lineDepartureTimePlanned1 = lineDepartureTimePlanned1;
+        this.lineDepartureTimePlanned2 = lineDepartureTimePlanned2;
     }
 
-    public LineRecord(String lineName, String lineTowards, String lineDepartureTimePlanned, String lineType, String lineId) {
-        this.lineName = lineName;
-        this.lineTowards = lineTowards;
-        this.lineDepartureTimePlanned = lineDepartureTimePlanned;
-        this.lineType = lineType;
-        this.lineId = lineId;
+    public String getLineTransportType() {
+        return lineTransportType;
     }
 
     public String getLineName() {
         return lineName;
     }
 
+    public String getLineStationName() {
+        return lineStationName;
+    }
+
     public String getLineTowards() {
         return lineTowards;
     }
 
-    public String getLineDepartureTimePlanned() {
-        return lineDepartureTimePlanned;
+    public String getLineDepartureTimePlanned1() {
+        return lineDepartureTimePlanned1;
     }
 
+    public String getLineDepartureTimePlanned2() {
+        return lineDepartureTimePlanned2;
+    }
 
     public String getLineType() {
         return lineType;
